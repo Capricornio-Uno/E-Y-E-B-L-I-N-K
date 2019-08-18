5dc14da68b0c94177b881847399a6f42330c03bf
Miguel A. Reinoso <miguelreinoso7@gmail.com>
2019-08-18 16:55:05 +0200

    Class 'Object' added

--- room1.py
+++ room1.py
@@ -9,17 +9,43 @@
 
 '''
 
-def three_blank_lines():
+# ====================================== Objects ============================================
+
+class Object:
+	def __str__(self):
+		return self.name
+
+class Handbag(Object):
+	def __init__(self):
+		self.name = "Handbag"
+		self.description = "It's a composite graphene bag. You store your things on it."
+		self.damage = 2
+	
+class Videophone(Object):
+	def __init__(self):
+		self.name = "Videophone"
+		self.description = "It's an old COM terminal."
+		self.damage = 3
+
+class ShockStick(Object):
+	def __init__(self):
+		self.name = "ShockStick"
+		self.description = "It's a self defense weapon. You always carry this with you."
+		self.damage = 8
+	
+# ========================================== Functions ========================================
+
+def three_blank_lines(): # Spaces between paragraphs
 	print()
 	print()
 	print()
 
-def get_player_command():
+def get_player_command(): # Player prompt
 	print()
 	return input("What now? ")
 
-def play():
-	inventory = ['Handbag','Videophone']
+def play(): # Main loop
+	inventory = [Handbag(),Videophone(),ShockStick()]
 	while True:
 		action_input = get_player_command()
 		if action_input == 'n':
@@ -37,6 +63,8 @@ def play():
 				print('*' + str(item))
 		else:
 			print("Invalid action")
+
+# ============================================== PROGRAM ============================================
 
 three_blank_lines()
 
